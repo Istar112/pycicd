5ac44b2485a7262aa5ffe09c5c80e3a373a8d7f2
istar112 <istarhf110809@gmail.com>
2026-01-23 20:18:14 +0100

    Arreglando el archivo setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="optativa_pycicd",
-    version="1.0.0",
+    name="pycicd_todo",
+    version="1.0.3",
     author="Istar Hernández",
     author_email="istarhf110809@gmail.com",
     description="Descripción de tu proyecto",
